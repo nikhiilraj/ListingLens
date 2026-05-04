588dc36c252f3b12a88a68b1dcea6e58bbc3da2c
Nikhil Raj <nikhil4rajj@gmail.com>
2026-05-04 12:01:08 +0530

    refactor: simplify devIndicators configuration by disabling all development indicators

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
+  devIndicators: false,
 };
 
 export default nextConfig;
